be8ce00c0fd8b184c533f9d3f504a4515ac44bbb
nemliProg <younesbuzz@gmail.com>
2022-10-26 21:08:09 +0100

    LogoutServlet created

---
+++ Promotion-Marjane/src/main/java/com/marjane/promotionmarjane/servlets/LogoutServlet.java
@@ -0,0 +1,20 @@
+package com.marjane.promotionmarjane.servlets;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", value = "/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();
+        }
+        request.getRequestDispatcher("/views/index.jsp").forward(request,response);
+    }
+}
