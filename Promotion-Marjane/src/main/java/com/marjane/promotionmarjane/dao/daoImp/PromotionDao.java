f4f0ae0a500e9d9abf6d4df20bb13f65ef60c577
nemliProg <younesbuzz@gmail.com>
2022-10-26 21:01:35 +0100

    getAllPromotions method created in PromotionDao Class

--- Promotion-Marjane/src/main/java/com/marjane/promotionmarjane/dao/daoImp/PromotionDao.java
+++ Promotion-Marjane/src/main/java/com/marjane/promotionmarjane/dao/daoImp/PromotionDao.java
@@ -1,7 +1,9 @@
 package com.marjane.promotionmarjane.dao.daoImp;
 
 import com.marjane.promotionmarjane.dao.AbstractHibernateDAO;;
+import com.marjane.promotionmarjane.entities.Categorie;
 import com.marjane.promotionmarjane.entities.Promotion;
+import org.hibernate.Session;
 
 import java.util.ArrayList;
 
@@ -20,6 +22,20 @@ public class PromotionDao extends AbstractHibernateDAO<Promotion>{
     @Override
     public void delete(Promotion entity) throws Exception {
         super.delete(entity);
+    }
+
+    public ArrayList<Promotion> getAllPromotions()  {
+        try {
+            Session session = getCurrentSession();
+            session.getTransaction();
+            session.beginTransaction();
+            ArrayList<Promotion> promotions = (ArrayList<Promotion>) session.createQuery("SELECT p FROM Promotion p", Promotion.class).getResultList();
+            session.getTransaction().commit();
+            session.close();
+            return promotions;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public ArrayList<Promotion> getAll() {
